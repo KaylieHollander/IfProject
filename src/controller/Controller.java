de9cca6b419f6684ebd157d6cc6ae914496563a6
Kaylie Hollander <Kphollander1@gmail.com>
2018-09-25 08:35:00 -0600

    Added while loops

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -9,20 +9,52 @@ public class Controller
 {
 
 	/**
-	 * Builds the instance of the Controller
+	 * Builds the instance of the Controller.
 	 */
-	
 	public Controller()
 	{
 		
 	}
 	
 	
-	
+	/**
+	 * This is where the program starts calling methods to run the program.
+	 */
 	public void start()
 	{
+		loops();
+	}
+	
+	
+	private void loops()
+	{
+		boolean isDone = false;
+		boolean isLlama = false;
+		int countDone = 0;
+		int countLlama = 0;
 		
-
+		while (!isDone)//test the variable
+		{
+			JOptionPane.showMessageDialog(null, "Dragon");
+//			Eventually change the loop variable
+			countDone++;
+			if (countDone >= 10)
+			{
+				isDone = true;	// update variable
+			}
+		}
+		
+		while (!isLlama)
+		{
+			JOptionPane.showMessageDialog(null, "Llama");
+			countLlama++;
+			if (countLlama >=15)
+			{
+				isLlama = true;
+			}
+			
+		}
+		
 	}
 		
 
--- src/model/Animals.java
+++ src/model/Animals.java
@@ -2,23 +2,34 @@ package model;
 
 public class Animals
 {
-
+// Declaration section
+//	Data member section
+//	Also called instance variables
+//	These are special b/c they get a default value
+//	All numbers => 0
+//	Booleans => false
+//	Objects => null
+	
 	private boolean elephants;
 	private double zebras;
 	private int llamas;
 	private double dragons;
 	
 	/**
-	 * Default constructor or after the fact customization
-	 * AKA I don't know the info yet about the Animals
+	 * Default constructor or after the fact customization.
+	 * AKA I don't know the info yet about the Animals.
+	 * Initializing data members with "bad" values to be changed.
 	 */
 	public Animals()
 	{
-		
+		this.elephants = true;
+		this.zebras = 99000;
+		this.llamas = 5;
+		this.dragons = 88;
 	}
 	
 	/**
-	 * Better constructor when I know the information for the animal object
+	 * Better constructor when I know the information for the animal object.
 	 * @param elephants
 	 * @param zebras
 	 * @param llamas
@@ -54,22 +65,22 @@ public class Animals
 	}
 	
 //	setters
-	public void setelephants()
+	public void setelephants(boolean elephants)
 	{
 		this.elephants = elephants;
 	}
 	
-	public void setzebras()
+	public void setzebras(double zebras)
 	{
 		this.zebras = zebras;
 	}
 	
-	public void setllamas()
+	public void setllamas(int llamas)
 	{
 		this.llamas = llamas;
 	}
 	
-	public void setdragons()
+	public void setdragons(double dragons)
 	{
 		this.dragons = dragons;
 	}
